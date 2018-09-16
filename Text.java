adb54fc61e9b10156e920a0d11254a44f6923f97
shangnannan <43309712+shangnannan@users.noreply.github.com>
2018-09-16 14:24:41 +0800

    Add files via upload

---
+++ Text.java
@@ -0,0 +1,13 @@
+
+public class Text {
+	public static void main(String[] args) {
+		for(int x=0;x<10;x++){
+			for(int y=0;y<5;y++){
+			if(y==3){
+				break;
+			}
+			System.out.println(x+","+y);
+		   }
+	   }
+	}
+}
